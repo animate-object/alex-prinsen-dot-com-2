7c0a529c8c92712468d3fa4f2e3c8cfd0636c4f6
Alex Prinsen <alexander.f.prinsen@gmail.com>
2024-10-26 21:21:47 -0600

    Navigable subheading anchors and style updates for the blog

--- src/js/blockRenderer.js
+++ src/js/blockRenderer.js
@@ -17,6 +17,33 @@
  * it would call itself on each child and append the result to the parent
  */
 
+/**
+ * Enable complex elements by specifying separate append and mount points
+ *
+ * The append point is where children should be appended
+ * The mount point is the element that should be appended to the parent
+ */
+const mountable = (appendEl, mountEl) => {
+  return {
+    appendPoint: appendEl,
+    mountPoint: mountEl ?? appendEl,
+  };
+};
+
+const isMountable = (el) => {
+  if (el.appendPoint && el.mountPoint) {
+    return true;
+  }
+  return false;
+};
+
+const toMountable = (el) => {
+  if (isMountable(el)) {
+    return el;
+  }
+  return mountable(el);
+};
+
 export const unhandledBlockRenderer = (block) => {
   const el = document.createElement("pre");
   el.innerText =
@@ -26,7 +53,14 @@ export const unhandledBlockRenderer = (block) => {
 
 const simpleRenderer = (tag) => (block) => {
   const el = document.createElement(tag);
+  if (tag.startsWith("h")) {
+    // debugging
+    console.log(el);
+  }
   el.innerText = block.text || "";
+  if (tag.startsWith("h")) {
+    console.log(el);
+  }
   return el;
 };
 
@@ -54,6 +88,36 @@ const listRenderer = (block) => {
 
 const headingRenderer = (block) => {
   return simpleRenderer(`h${block.level}`)(block);
+};
+
+const anchorifiedHeadingRenderer = (block) => {
+  const heading = headingRenderer(block);
+
+  const text = block.children?.[0]?.text;
+  // unexpected scenario, but handle it gracefully
+  if (!text) {
+    console.warn("No text found in heading block", block);
+    return heading;
+  }
+
+  const slug = text
+    .replace(/[^\w\s]/g, "") // remove non-alphanumeric characters
+    .split(" ")
+    .slice(0, 6)
+    .join("-")
+    .toLowerCase();
+
+  const anchor = document.createElement("a");
+  anchor.href = `#${slug}`;
+  anchor.id = slug;
+
+  heading.appendChild(anchor);
+  heading.classList.add("post-heading");
+
+  return {
+    appendPoint: anchor,
+    mountPoint: heading,
+  };
 };
 
 const linkRenderer = (block) => {
@@ -86,7 +150,7 @@ const BLOCK_RENDERERS = {
   text: textRenderer,
   "list-item": simpleRenderer("li"),
   list: listRenderer,
-  heading: headingRenderer,
+  heading: anchorifiedHeadingRenderer,
   code: simpleRenderer("code"),
   quote: simpleRenderer("blockquote"),
   link: linkRenderer,
@@ -100,10 +164,15 @@ const chooseBlockRenderer = (block) => {
 };
 
 export const createBlockEl = (block) => {
-  // create just this block
-
   const renderer = chooseBlockRenderer(block);
   return renderer(block);
+};
+
+const appendChild = (parent, child) => {
+  const mountableParent = toMountable(parent);
+  const mountableChild = toMountable(child);
+
+  mountableParent.appendPoint.appendChild(mountableChild.mountPoint);
 };
 
 export const renderBlock = (block) => {
@@ -111,12 +180,13 @@ export const renderBlock = (block) => {
 
   if (block.children) {
     block.children.forEach((child) => {
-      el.appendChild(renderBlock(child));
+      const childEl = renderBlock(child);
+      appendChild(el, childEl);
     });
   }
   return el;
 };
 
 export const renderAllBlocks = (blocks) => {
-  return blocks.map(renderBlock);
+  return blocks.map(renderBlock).map((el) => toMountable(el).mountPoint);
 };
--- src/js/post.js
+++ src/js/post.js
@@ -1,15 +1,38 @@
 import { findOnePost } from "./api.js";
-import { renderAllBlocks } from "./blockerRenderer.js";
+import { renderAllBlocks } from "./blockRenderer.js";
 
 const getTitleEl = () => document.getElementById("post-title");
 const getContentEl = () => document.getElementById("post-content");
 
-const getHash = () => {
-  return window.location.hash.slice(1);
+const getPostSlug = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const rawSlug = urlParams.get("post");
+  if (!rawSlug || rawSlug === "") {
+    return null;
+  }
+  // check matches the pattern of a valid slug (alphanumeric kebab-case)
+  if (!/^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(rawSlug)) {
+    return null;
+  }
+  return rawSlug;
 };
 
 const loadPost = (slug) => {
   return findOnePost(slug);
+};
+
+const scrollToHash = () => {
+  const hash = window.location.hash;
+  if (!hash) {
+    return;
+  }
+  const anchorEl = document.getElementById(hash.slice(1));
+  if (!anchorEl) {
+    console.warn(`No element found with id: ${hash}`);
+    return;
+  }
+  console.debug("Scrolling to hash:", hash);
+  anchorEl.scrollIntoView();
 };
 
 const renderPost = (post) => {
@@ -18,17 +41,31 @@ const renderPost = (post) => {
   const titleEl = getTitleEl();
   titleEl.innerText = title;
 
-  // set window title
   document.title = `Blue Blog - ${title}`;
 
   const blockEls = renderAllBlocks(content);
   blockEls.forEach((el) => postEl.appendChild(el));
+
+  scrollToHash();
+};
+
+const renderError = (message) => {
+  const postEl = getContentEl();
+
+  const errorEl = document.createElement("h2");
+  errorEl.innerText = message;
+
+  postEl.appendChild(errorEl);
 };
 
 const loadCurrentPost = () => {
-  const slug = getHash();
+  const slug = getPostSlug();
+  if (!slug) {
+    renderError("Invalid post identifier");
+    return;
+  }
+
   loadPost(slug).then((post) => {
-    console.log(post);
     renderPost(post);
   });
 };
